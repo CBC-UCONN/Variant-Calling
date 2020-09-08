baada35135936d3fe332428cd05fd84e93a0aa1b
Noah Reid <noah.reid@uconn.edu>
2020-09-08 16:00:20 -0400

    gatk

--- scripts/Part4c_gatk_gvcf.sh
+++ scripts/Part4c_gatk_gvcf.sh
@@ -24,47 +24,43 @@ module load GATK/4.0
 module load htslib
 module load bedtools
 
-mkdir -p ../variants_gatk
+INDIR=../align_pipe
 
-# make the targets file
-cd ../coverage_stats
-# select the regions we wish to call variants on, merge them using bedtools
-tabix ../coverage_stats/coverage_1kb.bed.gz chr20:29400000-34400000  | awk '$6 > 850 && $6 < 2550' | bedtools merge >../coverage_stats/targets.bed
- # it turns out we could use the outlier bed with -XL instead of making a target file and using -L
-
-# change directory to the bam file location
-cd ../align_pipe
+OUTDIR=../variants_gatk
+mkdir -p $OUTDIR
 
 # make a list of bam files
-ls *bam >bam.list
+ls $INDIR/*bam >$OUTDIR/bam.list
+
+TARGETS=../coverage_stats/targets.bed.gz
 
 # set a variable for the reference genome location
-GEN=/UCHC/PublicShare/Variant_Detection_Tutorials/Variant-Detection-Introduction-GATK_all/resources_all/Homo_sapiens_assembly38.fasta
+GEN=/UCHC/PublicShare/CBC_Tutorials/Variant_Detection_Tutorials/Variant-Detection-Introduction-GATK_all/resources_all/Homo_sapiens_assembly38.fasta
 
 gatk HaplotypeCaller \
      -R $GEN \
-     -I son.bam \
+     -I $INDIR/son.bam \
      -ERC GVCF \
-     -L ../coverage_stats/targets.bed \
-     --output ../variants_gatk/son.g.vcf
+     -L $TARGETS \
+     --output $OUTDIR/son.g.vcf
 
 date 
 
 gatk HaplotypeCaller \
      -R $GEN \
-     -I mom.bam \
+     -I $INDIR/mom.bam \
      -ERC GVCF \
-     -L ../coverage_stats/targets.bed \
-     --output ../variants_gatk/mom.g.vcf
+     -L $TARGETS \
+     --output $OUTDIR/mom.g.vcf
 
 date 
 
 gatk HaplotypeCaller \
      -R $GEN \
-     -I dad.bam \
+     -I $INDIR/dad.bam \
      -ERC GVCF \
-     -L ../coverage_stats/targets.bed \
-     --output ../variants_gatk/dad.g.vcf
+     -L $TARGETS \
+     --output $OUTDIR/dad.g.vcf
 
 date
 
