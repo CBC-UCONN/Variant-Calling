470971192f711531f1912bfe0fe0009152b3a50f
Noah Reid <noah.reid@uconn.edu>
2020-09-09 09:28:43 -0400

    filter

--- scripts/Part5a_filter.sh
+++ scripts/Part5a_filter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=bcf_vcf
+#SBATCH --job-name=filter_vcfs
 #SBATCH -n 1
 #SBATCH -N 1
 #SBATCH -c 1
@@ -16,13 +16,17 @@ date
 
 module load bcftools/1.6
 module load htslib
+module load bedtools/2.29.0
 
 OUTDIR=../filtered_vcfs
 mkdir -p $OUTDIR
 
+TARGETS=../coverage_stats/targets.bed
+
 bcftools filter -s LowQual -e '%QUAL<50' ../variants_freebayes/chinesetrio_fb.vcf.gz | bgzip -c > $OUTDIR/fb_filter.vcf.gz
 bcftools filter -s LowQual -e '%QUAL<50' ../variants_gatk/chinesetrio.vcf.gz | bgzip -c > $OUTDIR/gatk_filter.vcf.gz
-bcftools filter -s LowQual -e '%QUAL<50' ../variants_bcftools/chinesetrio.vcf.gz | bgzip -c > $OUTDIR/bcf_filter.vcf.gz
+# additionally filter bcftools output to match targets used for gatk and freebayes
+bcftools filter -s LowQual -e '%QUAL<50' ../variants_bcftools/chinesetrio.vcf.gz | bedtools intersect -wa -a stdin -b $TARGETS | bgzip -c > $OUTDIR/bcf_filter.vcf.gz
 
 for file in $OUTDIR/*vcf.gz
 do tabix -f -p vcf $file
